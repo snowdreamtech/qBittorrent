c331e8bb5285849434136c7e5f87ed4ebea05596
snowdream <sn0wdr1am@qq.com>
2024-07-13 20:21:21 +0800

    feat: 4.6.4

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ GITHUB_PROJECT=ghcr.io/snowdreamtech/qbittorrent
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm64,linux/ppc64le,linux/riscv64 \
 -t ${DOCKER_HUB_PROJECT}:flood-latest \
--t ${DOCKER_HUB_PROJECT}:flood-4.6.5 \
+-t ${DOCKER_HUB_PROJECT}:flood-4.6.4 \
 -t ${DOCKER_HUB_PROJECT}:flood-4.6 \
 -t ${DOCKER_HUB_PROJECT}:flood-4 \
 -t ${GITHUB_PROJECT}:flood-latest \
--t ${GITHUB_PROJECT}:flood-4.6.5 \
+-t ${GITHUB_PROJECT}:flood-4.6.4 \
 -t ${GITHUB_PROJECT}:flood-4.6 \
 -t ${GITHUB_PROJECT}:flood-4 \
 . \
