371f9533a0d83ce1b9373219eac298adca93ed25
snowdream <sn0wdr1am@qq.com>
2024-07-19 23:19:36 +0800

    feat: shell format

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ DOCKER_HUB_PROJECT=snowdreamtech/qbittorrent
 GITHUB_PROJECT=ghcr.io/snowdreamtech/qbittorrent
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
--t ${DOCKER_HUB_PROJECT}:latest \
--t ${DOCKER_HUB_PROJECT}:4.6.4 \
--t ${DOCKER_HUB_PROJECT}:4.6 \
--t ${DOCKER_HUB_PROJECT}:4 \
--t ${GITHUB_PROJECT}:latest \
--t ${GITHUB_PROJECT}:4.6.4 \
--t ${GITHUB_PROJECT}:4.6 \
--t ${GITHUB_PROJECT}:4 \
-. \
---push
+    -t ${DOCKER_HUB_PROJECT}:latest \
+    -t ${DOCKER_HUB_PROJECT}:4.6.4 \
+    -t ${DOCKER_HUB_PROJECT}:4.6 \
+    -t ${DOCKER_HUB_PROJECT}:4 \
+    -t ${GITHUB_PROJECT}:latest \
+    -t ${GITHUB_PROJECT}:4.6.4 \
+    -t ${GITHUB_PROJECT}:4.6 \
+    -t ${GITHUB_PROJECT}:4 \
+    . \
+    --push
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,15 +5,15 @@ set -e
 if [ -n "${WEBUI_USER}" ] && [ -n "${WEBUI_PASS}" ]; then
     # username
     sed -i "s/WebUI\\\Username.*/WebUI\\\Username=${WEBUI_USER}/g" /var/lib/qBittorrent/config/qBittorrent.conf
-    
+
     # password
     HASH=$(/var/lib/qBittorrent/bin/passwd "${WEBUI_PASS}")
-    
+
     sed -i "s|WebUI\\\Password_PBKDF2.*|WebUI\\\Password_PBKDF2=${HASH}|g" /var/lib/qBittorrent/config/qBittorrent.conf
 else
     # username
     sed -i "s/WebUI\\\Username.*/WebUI\\\Username=admin/g" /var/lib/qBittorrent/config/qBittorrent.conf
-    
+
     # password
     HASH=""
     sed -i "s/WebUI\\\Password_PBKDF2.*/WebUI\\\Password_PBKDF2=\"${HASH}\"/g" /var/lib/qBittorrent/config/qBittorrent.conf
@@ -34,7 +34,7 @@ if [ -n "${PEER_PORT}" ]; then
     sed -i "s/Session\\\Port.*/Session\\\Port=${PEER_PORT}/g" /var/lib/qBittorrent/config/qBittorrent.conf
 fi
 
-# qbittorrent-nox 
+# qbittorrent-nox
 qbittorrent-nox --profile=/var/lib
 
 # exec commands
